81de10d359462081cfcf706e805d36efa152fe98
Charlie Tribble <cmtribble3@gmail.com>
2020-02-08 19:37:30 -0500

    Not what I had in mind but we got there

---
+++ rio/2020fv/src/main/java/frc/robot/Indexing.java
@@ -0,0 +1,66 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class Indexing {
+    protected TalonSRX bottomBeltIndex;
+    protected TalonSRX frontUpperBeltIndex;
+    protected TalonSRX frontLowerBeltIndex;
+    protected TalonSRX backBeltIndex;
+
+    // 0 is top, 4 is intake
+    protected DigitalInput ballPos0;
+    protected DigitalInput ballPos1;
+    protected DigitalInput ballPos2;
+    protected DigitalInput ballPos3;
+    protected DigitalInput ballPos4;
+
+    protected OI oi;
+
+    public Indexing(OI oi) {
+        bottomBeltIndex = new TalonSRX(RobotMap.INDEX_BOTTOM_BELT_CANID);
+        frontUpperBeltIndex = new TalonSRX(
+                RobotMap.INDEX_FRONT_UPPER_BELT_CANID);
+        frontLowerBeltIndex = new TalonSRX(
+                RobotMap.INDEX_FRONT_LOWER_BELT_CANID);
+        backBeltIndex = new TalonSRX(RobotMap.INDEX_BACK_BELT_CANID);
+
+        ballPos0 = new DigitalInput(0);
+        ballPos1 = new DigitalInput(1);
+        ballPos2 = new DigitalInput(2);
+        ballPos3 = new DigitalInput(3);
+        ballPos4 = new DigitalInput(4);
+
+        this.oi = oi;
+    }
+
+    public void indexTeleopPeriodic() {
+
+    }
+
+    public void runBottomBelt(double speed) {
+        bottomBeltIndex.set(ControlMode.PercentOutput, speed);
+    }
+
+    public void runFrontUpperBelt(double speed) {
+        bottomBeltIndex.set(ControlMode.PercentOutput, speed);
+    }
+
+    public void runFrontLowerBelt(double speed) {
+        bottomBeltIndex.set(ControlMode.PercentOutput, speed);
+    }
+
+    public void runBackBelt(double speed) {
+        bottomBeltIndex.set(ControlMode.PercentOutput, speed);
+    }
+
+    public void loadShooter() {
+
+    }
+
+    public boolan hasBalls() {
+        return true;
+    }
+}
---
+++ rio/2020fv/src/main/java/frc/robot/Intake.java
@@ -0,0 +1,68 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+public class Intake {
+
+    protected DoubleSolenoid intakeFlipper;
+    protected TalonSRX intakeMotor;
+
+    protected int multi;
+    protected OI oi;
+
+    protected boolean flipButtonLast = false;
+    protected boolean flipButton = false;
+
+    public Intake(OI oi) {
+        intakeFlipper = new DoubleSolenoid(RobotMap.DS_FORWARD_CHANNEL,
+                RobotMap.DS_REVERSE_CHANNEL);
+        intakeMotor = new TalonSRX(RobotMap.INTAKE_CANID);
+
+        multi = 1;
+        this.oi = oi;
+    }
+
+    public void intakeTeleopPeriodic() {
+        if (oi.getLeftStickButton(RobotMap.JOYSTICK_TRIGGER)) {
+            if (oi.getLeftStickButton(RobotMap.JOYSTICK_LEFT_BUTTON)) {
+                intakeReverse();
+            }
+            else {
+                intakeIn();
+            }
+        }
+        else {
+            intakeStop();
+        }
+
+        flipButton = oi.getLeftStickButton(RobotMap.JOYSTICK_CENTER_BUTTON);
+
+        flipIntake(flipButton && !flipButtonLast);
+
+        flipButtonLast = flipButton;
+
+    }
+
+    public void flipIntake(boolean flip) {
+        if (flip) {
+            if (intakeFlipper.get().equals(DoubleSolenoid.Value.kReverse))
+                intakeFlipper.set(DoubleSolenoid.Value.kForward);
+            else intakeFlipper.set(DoubleSolenoid.Value.kReverse);
+        }
+    }
+
+    public void intakeIn() {
+        intakeMotor.set(ControlMode.PercentOutput, 0.75);
+    }
+
+    public void intakeReverse() {
+        intakeMotor.set(ControlMode.PercentOutput, -0.75);
+    }
+
+    public void intakeStop() {
+        intakeMotor.set(ControlMode.PercentOutput, 0);
+    }
+
+}
